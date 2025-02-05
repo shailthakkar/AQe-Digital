7bd0728de523367cc2fd66ba76079086084fa878
Shail Thakkar <108196534+shailthakkar@users.noreply.github.com>
2025-02-05 14:46:13 +0530

    Add files via upload

---
+++ Homerun Hub/tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // add this line
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customGreen: "rgb(26 144 7)", // Add your custom color
+        customGray: "rgb(8 15 37)",
+      },
+    },
+  },
+  plugins: [],
+};
